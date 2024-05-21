1ca5217d09769d731acfda83500e02e8fa7d9a62
Julita <jvferreriro@escuelasproa.edu.ar>
2024-05-21 09:20:21 -0300

    clereon

--- seed.js
+++ seed.js
@@ -56,8 +56,6 @@ const athlon = new PcArmada({
     }
 })
 
-
-
 const amdRyzen3 = new PcArmada({
     name: "AMD Ryzen 3",
     cost: 297000,
@@ -128,10 +126,47 @@ const amdRyzen5 = new PcArmada({
         uso: ['Hogar', 'Arquitectura', 'Diseño']
     }
 })
+const celeron = new PcArmada({
+    name: "PC INTEL CELERON DUAL",
+    cost: 999999,
+    description: "Este equipo fue ensamblado por OrangeBox. PC ideal para uso hogareño y de oficina, excelente relación entre precio y potencia."
+    ,
+    caracteristicas:{
+        almacenamiento:{
+            discoDuro: false,
+            interfaz: "Sata 2.5",
+            discoSolido: 240
+        },
+        conectividad:{
+            wifi: true,
+            ethernet: true,
+        },
+        memoria:{
+            capacidad: 4,
+            expandible: true
+        },
+        procesador:{
+            fabricante: 'Athlon',
+            modelo: "3000G",
+            nucleos:{
+                nucle: 2,
+                subprocesos: 4
+            },
+            frecuencia: 3.5,
+            cache: "4Mb StamartCache"
+        },
+        puertos:{
+            usb: true,
+            hdmi: true,
+            audio: true
+        },
+        uso: ['Hogar', 'Gaming', 'Arquitectura']
+    }
+})
+
 
 // Array with all PC's
-const allPcs = [amdRyzen3, athlon, amdRyzen5]
-
+const allPcs = [athlon, celeron, amdRyzen3, amdRyzen5]
 
 // Saving
 PcArmada.insertMany(allPcs)
