d368f94d192e229c216fbf70babb3f45713fa0c0
IanButcher <115805201+IanButcher@users.noreply.github.com>
2024-05-21 11:31:02 -0300

    Update index.js

--- index.js
+++ index.js
@@ -14,57 +14,10 @@ mongoose.connect('mongodb://localhost:27017/orangebox')
         console.log(err)
     })
 
-
 // Function await saving
 const awaitSave = async (object) =>{
     await object.save()
 }
-
-// Ejemplo de pc armada (no borrar)
-const kairosPro = new PcArmada({
-    name: "KairosPro",
-    cost: 1351447,
-    description: "Este equipo fue ensamblado por OrangeBox",
-    caracteristicas:{
-        almacenamiento:{
-            discoDuro: true,
-            interfaz: "Sata 2.5",
-            discoSolido: 240
-        },
-        conectividad:{
-            wifi: true,
-        },
-        graficos:{
-            motor: "Asus Nvidia",
-            modelo: "RTX 4060Ti",
-            vram: 8,
-            memoria: "GDDR6"
-        },
-        memoria:{
-            capacidad: 16,
-            expandible: true
-        },
-        procesador:{
-            fabricante: 'AMD',
-            modelo: "4000G",
-            nucleos:{
-                nucle: 4,
-                subprocesos: 8
-            },
-            frecuencia: 4,
-            cache: "4Mb cache"
-        },
-        puertos:{
-            usb: true,
-            hdmi: true,
-            audio: true
-        },
-        uso: ['Hogar', 'Gaming', 'Arquitectura']
-    }
-})
-
-// Saving kariosPro into pcArmadas table
-awaitSave(kairosPro)
 
 // Server config
 const app = express()
@@ -93,6 +46,7 @@ app.get('/register', (req, res)=>{
 })
 
 app.get('/pc-armadas', (req, res)=>{
+    // {pcArray requets from db falta hacer jsakdjsdkajsdkjskdj}
     res.render('pcArmadas.ejs')
 })
 
