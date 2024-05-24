68ecf57676cee6151a4be1aa6b960ca746487933
IanButcher <iancorradibutcher@gmail.com>
2024-05-23 23:47:21 -0300

    components add new and components edit (on process)

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose')
 const PcArmada = require('./models/pcArmada')
 const Product = require('./models/product')
 const User = require('./models/users')
+const methodOverride = require('method-override')
 
 // DB conection
 mongoose.connect('mongodb://localhost:27017/orangebox')
@@ -15,7 +16,7 @@ mongoose.connect('mongodb://localhost:27017/orangebox')
         console.log(err)
     })
 
-// Function await saving
+// Function await saving just to troll
 const awaitSave = async (object) =>{
     await object.save()
 }
@@ -24,8 +25,12 @@ const awaitSave = async (object) =>{
 const app = express()
 const port = 8080
 
+// Override
+app.use(methodOverride('_method'))
+
 // View engine
 app.set('view engine', 'ejs')
+app.use(express.urlencoded({extended: true}))
 
 // Path
 const path = require('path')
@@ -73,6 +78,21 @@ app.get('/components', async (req, res) => {
     
 })
 
+// Arrays of categories
+const tipito = ['Microprocesador', 'Motherboard', 'Ram', 'Disco Duro', 'Disco SSD', 'Placa de Video', 'Gabinete', 'Cooler']
+const fabricantesito = ['AMD', 'Intel', 'ASRock', 'ASUS', 'NVIDIA', 'Crucial', 'Patriot', 'MarkVision', 'Fury', 'Seagate', 'WesternDigital', 'HIK', 'ADATA', 'Gamdias', 'Red Dragon', 'PowerColor', 'GigaByte', 'Corsair', 'Thermaltake', 'CoolerMaster', 'Vengeance' ]
+
+// Create
+app.get('/components/im-an-admin-doing-stuff', (req, res) => {
+    res.render('components/new.ejs', { fabricantesito, tipito })
+})
+
+app.post('/components', async (req, res) => {
+    const newComponent = new Product(req.body)
+    awaitSave(newComponent)
+    res.redirect(`/components/${newComponent._id}`)
+})
+
 app.get('/components/:id', async (req, res) => {
     try{
         const { id } = req.params
@@ -81,6 +101,18 @@ app.get('/components/:id', async (req, res) => {
     } catch (err){
         console.log(err)
     }
+})
+
+app.get('/components/:id/im-an-admin-doing-stuff', async (req, res) => {
+    const { id } = req.params
+    const component = await Product.findById(id)
+    res.render('components/edit.ejs', {component, fabricantesito, tipito })
+})
+
+app.put('/components/:id', async (req, res) => {
+    const { id } = req.params
+    const component = await Product.findByidAndUpdate(id, req.body, {runValidators: true})
+    res.redirect(`/components/${component._id}`, {component})
 })
 
 // Other routes
